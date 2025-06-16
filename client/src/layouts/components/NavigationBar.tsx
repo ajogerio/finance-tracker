6faba27878adbb859c042d52d00bc38d0891f99c
ajogerio <aaogerio@up.edu.ph>
2025-06-17 00:00:09 +0800

    feat: add logout button

--- client/src/layouts/components/NavigationBar.tsx
+++ client/src/layouts/components/NavigationBar.tsx
@@ -16,22 +16,28 @@ export default function NavigationBar() {
   return (
     <nav
       className="flex flex-col items-center min-w-40 bg-[#fdfffe] rounded-tr-lg 
-    rounded-br-lg shadow-2xl gap-3"
+    rounded-br-lg shadow-2xl"
     >
       {/* logo  */}
       <h1 className="flex justify-center items-center min-h-15 ">
         CookieCoins
       </h1>
 
-      {/* navigation buttons  */}
-      <div className="flex flex-col max-w-[180px] gap-5 text-gray-600">
-        {navigationData.map((data) => (
-          <NavigationButton
-            key={data.label}
-            label={data.label}
-            icon={data.icon}
-          />
-        ))}
+      {/* navigation and logout buttons container  */}
+      <div className="flex flex-col flex-1 max-w-[180px] gap-5 text-gray-600 justify-between my-5">
+        {/* top navigation buttons  */}
+        <div className="flex flex-col gap-5">
+          {navigationData.map((data) => (
+            <NavigationButton
+              key={data.label}
+              label={data.label}
+              icon={data.icon}
+            />
+          ))}
+        </div>
+
+        {/* bottom logout button  */}
+        <NavigationButton label={"Logout"} icon={"fa-right-from-bracket"} />
       </div>
     </nav>
   );
