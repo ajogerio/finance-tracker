322a4da547f434a30cdf90a3bc12fe144fa1d806
ajogerio <aaogerio@up.edu.ph>
2025-06-17 00:16:53 +0800

    feat: add profile picture and account details in header

--- client/src/layouts/components/Header.tsx
+++ client/src/layouts/components/Header.tsx
@@ -1,3 +1,21 @@
+import cb1 from "../../assets/cb1.jpeg";
+
 export default function Header() {
-  return <header className="bg-gray-100 min-h-15">Header</header>;
+  return (
+    <header className="flex justify-end items-center bg-gray-100 min-h-15 gap-3 pr-5">
+      {/* profile picture */}
+      <img
+        src={cb1}
+        alt="Profile picture"
+        className="w-10 h-10 rounded-full object-cover"
+      />
+
+      {/* name and email  */}
+      <div className="flex flex-col">
+        <h1 className="font-bold text-xs">Christy Lagapa</h1>
+        <p className="text-xs text-gray-600">thaliaty@gmail.com</p>
+      </div>
+      <i className="fa-solid fa-caret-down text-gray-600"></i>
+    </header>
+  );
 }
