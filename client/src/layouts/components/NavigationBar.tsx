23281af3d304bdf7d3f1e96d40666441f9970360
ajogerio <aaogerio@up.edu.ph>
2025-06-16 23:43:12 +0800

    feat: add icons for each navigation button

--- client/src/layouts/components/NavigationBar.tsx
+++ client/src/layouts/components/NavigationBar.tsx
@@ -1,10 +1,15 @@
 import NavigationButton from "./NavigationButton";
 
-const navigationLabels: string[] = [
-  "Dashboard",
-  "Transactions",
-  "Budgeting",
-  "Savings",
+type NavigationItem = {
+  label: string;
+  icon: string;
+};
+
+const navigationData: NavigationItem[] = [
+  { label: "Dashboard", icon: "fa-compass" },
+  { label: "Transactions", icon: "fa-coins" },
+  { label: "Budgeting", icon: "fa-chart-pie" },
+  { label: "Savings", icon: "fa-piggy-bank" },
 ];
 
 export default function NavigationBar() {
@@ -13,10 +18,19 @@ export default function NavigationBar() {
       className="flex flex-col items-center min-w-40 bg-[#fdfffe] rounded-tr-lg 
     rounded-br-lg shadow-2xl gap-3"
     >
-      <h1 className="flex justify-center items-center min-h-15 ">LOGO</h1>
-      <div className="flex flex-col max-w-[180px] gap-5">
-        {navigationLabels.map((label) => (
-          <NavigationButton key={label} label={label} />
+      {/* logo  */}
+      <h1 className="flex justify-center items-center min-h-15 ">
+        CookieCoins
+      </h1>
+
+      {/* navigation buttons  */}
+      <div className="flex flex-col max-w-[180px] gap-5 text-gray-600">
+        {navigationData.map((data) => (
+          <NavigationButton
+            key={data.label}
+            label={data.label}
+            icon={data.icon}
+          />
         ))}
       </div>
     </nav>
--- client/src/layouts/components/NavigationButton.tsx
+++ client/src/layouts/components/NavigationButton.tsx
@@ -1,11 +1,15 @@
 type NavigationButtonProps = {
   label: string;
+  icon: string;
 };
 
-export default function NavigationButton({ label }: NavigationButtonProps) {
+export default function NavigationButton({
+  label,
+  icon,
+}: NavigationButtonProps) {
   return (
     <button className="flex items-center gap-4 w-full text-left">
-      <i className="fa-regular fa-compass"></i>
+      <i className={`fa-solid ${icon}`}></i>
       <p className="text-xs">{label}</p>
     </button>
   );
